e05baba540e057d0ac6d7d9b13b8d1cd1b1e1288
Marcus Sabon <marcus.sabon@etu.univ-grenoble-alpes.fr>
2024-10-07 20:55:14 +0200

    helloworld avec polytech package

--- hello.world/src/edu/polytech/oop/HelloWorld.java
+++ hello.world/src/edu/polytech/oop/HelloWorld.java
@@ -1,4 +1,6 @@
-  public class HelloWorld {
+  
+ package edu.polytech.oop; 
+ public class HelloWorld {
     public static void main(String[] args) {
       System.out.println("Hello World!");
       return;
