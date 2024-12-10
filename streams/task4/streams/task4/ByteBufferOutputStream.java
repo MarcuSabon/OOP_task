69bb486b2f9e55168f2b2d6b72f801c98d8c9372
Marcus Sabon <marcus.sabon@etu.univ-grenoble-alpes.fr>
2024-12-10 17:35:19 +0100

    amelioration step 2

--- streams/task4/streams/task4/ByteBufferOutputStream.java
+++ streams/task4/streams/task4/ByteBufferOutputStream.java
@@ -2,37 +2,59 @@ package streams.task4;
 
 public class ByteBufferOutputStream extends ByteArrayOutputStream {
 
-	private final int chunkSize; 
 	private Chunk head; 
 	private Chunk tail; 
+	private int compteur;
 
 	public ByteBufferOutputStream(int chunkSize) {
 		super(new byte[chunkSize]); 
-		this.chunkSize = chunkSize;
-		this.head = new Chunk(buffer); 
+		this.head = null; 
 		this.tail = head;
+		this.compteur = 0;
 
 	}
 	@Override
 	public void write(byte value) {
-		if (available()!=0) {
+		buffer[position] = value;
+		position++;
+		compteur++;
+		if (available()==0) {
             agrandir();
         }
-        buffer[position++] = value;
 	}
+	
+	@Override
+	public byte[] getBytes() {
+		while(head!=null) {
+			buffer = head.get_bytes(); // changer ecrase le buffer
+			head = head.get_next();
+		}
+		return buffer;
+	}
+	
+	@Override
+	public int getSize() {
+		return compteur;
+	}
+	
 	public void agrandir() {
-		Chunk newChunk = new Chunk(new byte[chunkSize]);
-		tail.next = newChunk;
-		tail = newChunk;
-		buffer = newChunk.bytes;
-		position = 0;
-		size += chunkSize;
-	}
-	
-	
-	
-	public Chunk header(){
-		return head;
-	}
+		Chunk newChunk = new Chunk(buffer);
+		if(head == null) {
+			head = newChunk;
+	        tail= head;
+	        head.set_bytes(buffer);
+	        position = 0;
+		}
+		else {
+			
+			tail.set_next(newChunk);
+	        tail.get_next().set_next(null);
+	        tail = tail.get_next();
+	        newChunk.set_bytes(buffer);
+	        position = 0;
+		}
+		
+        
+    }
 }
 
