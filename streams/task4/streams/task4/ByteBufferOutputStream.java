836e7fdc0bfd9ca4004b7b71ac9978a633c7fc7c
Marcus Sabon <marcus.sabon@etu.univ-grenoble-alpes.fr>
2024-12-11 09:44:37 +0100

    Sprint stream, task4, step2 completed

--- streams/task4/streams/task4/ByteBufferOutputStream.java
+++ streams/task4/streams/task4/ByteBufferOutputStream.java
@@ -2,59 +2,81 @@ package streams.task4;
 
 public class ByteBufferOutputStream extends ByteArrayOutputStream {
 
-	private Chunk head; 
-	private Chunk tail; 
+	private Chunk head;
+	private Chunk tail;
 	private int compteur;
 
 	public ByteBufferOutputStream(int chunkSize) {
-		super(new byte[chunkSize]); 
-		this.head = null; 
+		super(new byte[chunkSize]);
+		this.head = null;
 		this.tail = head;
 		this.compteur = 0;
 
 	}
+
 	@Override
 	public void write(byte value) {
 		buffer[position] = value;
 		position++;
 		compteur++;
-		if (available()==0) {
-            agrandir();
-        }
+		if (available() == 0) {
+			agrandir();
+		}
 	}
-	
+
 	@Override
 	public byte[] getBytes() {
-		while(head!=null) {
-			buffer = head.get_bytes(); // changer ecrase le buffer
-			head = head.get_next();
+		if (position > 0) {
+			byte[] last_bytes = new byte[position];
+		    for (int i = 0; i < position; i++) {
+		        last_bytes[i] = buffer[i];
+		    } 
+	        Chunk newChunk = new Chunk(last_bytes);
+
+	        if (head == null) {
+	            head = newChunk;
+	            tail = head;
+	        } else {
+	            tail.set_next(newChunk);
+	            tail = newChunk;
+	        }
+	    }
+		
+		
+		byte[] liste_fin = new byte[compteur];
+		Chunk current = head;
+		int index = 0;
+		while (current != null) {
+			byte[] currentBytes = current.get_bytes();
+			for (int i = 0; i < currentBytes.length && index < liste_fin.length; i++) {
+				liste_fin[index++] = currentBytes[i];
+			}
+			current = current.get_next();
 		}
-		return buffer;
+
+		return liste_fin;
 	}
-	
+
 	@Override
 	public int getSize() {
 		return compteur;
 	}
-	
-	public void agrandir() {
-		Chunk newChunk = new Chunk(buffer);
-		if(head == null) {
-			head = newChunk;
-	        tail= head;
-	        head.set_bytes(buffer);
-	        position = 0;
-		}
-		else {
-			
-			tail.set_next(newChunk);
-	        tail.get_next().set_next(null);
-	        tail = tail.get_next();
-	        newChunk.set_bytes(buffer);
-	        position = 0;
-		}
-		
-        
-    }
-}
 
+	public void agrandir() {
+
+		Chunk newChunk = new Chunk(buffer);
+		if (head == null) {
+			head = newChunk;
+			tail = head;
+
+		} else {
+
+			tail.set_next(newChunk);
+			tail.get_next().set_next(null);
+			tail = tail.get_next();
+		}
+		buffer = new byte[buffer.length];
+		position = 0;
+
+	}
+}
